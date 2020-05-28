05f77c1922429a09ede222fa41e3779769263eed
Stuart Schechter <stuart.schechter@gmail.com>
2020-05-28 15:07:40 +0900

    fix bug in binding macros

--- cpp/binding-macros.hpp
+++ cpp/binding-macros.hpp
@@ -3,19 +3,19 @@
 #include <emscripten/bind.h>
 #include "binding-helpers.hpp"
 
-template<typename T>
-inline std::string toJsonSimple(T jsonSerializable) {
-  return jsonSerializable.toJson();
-}
 
-template<typename T>
-inline emscripten::val toSerializedBinaryForm(T binarySerializable) {
-  return toJsUint8Array(binarySerializable.toSerializedBinaryForm());
-}
 
 #define AddSerializable(CLASSTYPE) \
-  .class_function("fromSerializedBinaryForm", &CLASSTYPE::fromSerializedBinaryForm) \
-  .function("toSerializedBinaryForm", &toSerializedBinaryForm<CLASSTYPE>) \
+  .class_function("fromSerializedBinaryForm", *[] \
+    (const emscripten::val serializedBinaryForm)->CLASSTYPE{ \
+      return CLASSTYPE::fromSerializedBinaryForm( \
+        sodiumBufferFromJsTypedNumericArray(serializedBinaryForm) \
+      ); \
+    }) \
+  .function("toSerializedBinaryForm",  *[] \
+    (const CLASSTYPE &binarySerializable)->emscripten::val{ \
+      return toJsUint8Array(binarySerializable.toSerializedBinaryForm()); \
+    }) \
   .class_function("fromJson", &CLASSTYPE::fromJson) \
   .function("toJson",  *[] \
     (const CLASSTYPE &jsonSerializable)->std::string{ return jsonSerializable.toJson(); }) \
--- src/tests/secret.test.ts
+++ src/tests/secret.test.ts
@@ -18,6 +18,33 @@ describe("Secret", () => {
         secret.delete();
     });
 
+    test('to and from binary form', async () => {
+        var module = await SeededCryptoModulePromise;
+        const secret = module.Secret.deriveFromSeed(seedString, derivationOptionsJson);
+        const copy = module.Secret.fromSerializedBinaryForm(secret.toSerializedBinaryForm())
+        expect(copy.derivationOptionsJson).toEqual(secret.derivationOptionsJson);
+        expect(copy.secretBytes).toEqual(secret.secretBytes);
+        secret.delete();
+    });
+
+    test('to and from json', async () => {
+        var module = await SeededCryptoModulePromise;
+        const secret = module.Secret.deriveFromSeed(seedString, derivationOptionsJson);
+        const copy = module.Secret.fromJson(secret.toJson())
+        expect(copy.derivationOptionsJson).toEqual(secret.derivationOptionsJson);
+        expect(copy.secretBytes).toEqual(secret.secretBytes);
+        secret.delete();
+    });
+
+    test('to and from custom json', async () => {
+        var module = await SeededCryptoModulePromise;
+        const secret = module.Secret.deriveFromSeed(seedString, derivationOptionsJson);
+        const copy = module.Secret.fromJson(secret.toCustomJson(2, "\t".charCodeAt(0)))
+        expect(copy.derivationOptionsJson).toEqual(secret.derivationOptionsJson);
+        expect(copy.secretBytes).toEqual(secret.secretBytes);
+        secret.delete();
+    });
+
     test('use argon2', async () => {
         var module = await SeededCryptoModulePromise;
         const oldSecret = module.Secret.deriveFromSeed(seedString, derivationOptionsJson);
